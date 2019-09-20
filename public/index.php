f3149f37c82a83629dff4c82667e71fbe6a7b3a2
SimonDevelop <contact@simon-micheneau.fr>
2019-09-20 15:15:06 +0200

    fix: Return to back config for static files

--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <?php
 
 if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
-
-    // check the file types, only serve standard files
-    if (preg_match('/\.(?:png|js|jpg|jpeg|gif|css)$/', $file)) {
-        // does the file exist? If so, return it
-        if (is_file($file))
-            return false;
-
-        // file does not exist. return a 404
-        header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-        printf('"%s" does not exist', $_SERVER['REQUEST_URI']);
+    if (is_file($file)) {
         return false;
     }
 }
