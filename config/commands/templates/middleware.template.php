8fa4f1f39c85dae50a929572a8daef1f45be9b7d
SimonDevelop <contact@simon-micheneau.fr>
2019-09-23 15:32:56 +0200

    feat: Update middleware template

--- config/commands/templates/middleware.template.php
+++ config/commands/templates/middleware.template.php
@@ -1,20 +1,23 @@
 <?php
+
 namespace App\Middlewares;
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class PregReplace
+class PregReplace implements Middleware
 {
-    private $twig;
+    private $container;
 
-    public function __construct(\Twig_Environment $twig)
+    public function __construct($container)
     {
-        $this->twig = $twig;
+        $this->container = $container;
     }
 
-    public function __invoke(Request $request, Response $response, $next)
+    public function process(Request $request, RequestHandler $handler): Response
     {
-        return $next($request, $response);
+        return $handler->handle($request);
     }
 }
