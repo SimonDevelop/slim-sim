258909a1716a127e3b6a90a557bdae1d5551df80
SimonDevelop <contact@simon-micheneau.fr>
2019-10-07 15:28:21 +0200

    feat: Config php server for static files outside index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
-
 use DI\Container;
 use Slim\Factory\AppFactory;
 use Dotenv\Dotenv;
