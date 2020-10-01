573be1186c95a4454471954f798889c9edcd612d
Simon Micheneau <contact@simon-micheneau.fr>
2020-10-01 12:03:39 +0200

    fix: Namespaces compatibility fix for datafixture

--- app/src/Entity/DataFixtures/UserFixture.php
+++ app/src/Entity/DataFixtures/UserFixture.php
@@ -4,7 +4,7 @@ namespace DataFixtures;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 use App\Entity\User;
 
